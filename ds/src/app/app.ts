badd1e77c72bc344bd9aa8b502c4cc413e1428b6
mares1402 <maresd1402@gmail.com>
2025-09-17 22:07:54 -0600

    V1.1

--- ds/src/app/app.ts
+++ ds/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrls: ['./app.css']  
-})
-export class App {
+export class Login {
+  protected readonly title = signal('ds');
 
+  email: string = '';
+  password: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login() {
+    const datos = {
+      correo: this.email,
+      contrasena: this.password
+    };
+
+    this.http.post('http://localhost:3000/api/auth/login', datos)
+      .subscribe({
+        next: (res: any) => {
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['/hola']);
+        },
+        error: () => {
+          alert('Credenciales incorrectas');
+        }
+      });
+  }
 }
